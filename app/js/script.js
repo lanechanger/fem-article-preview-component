7236401265f8ea4d4cc641c771fc83a30d9b00ab
lanechanger <jeremy_ng_86@live.ca>
2021-06-14 23:21:06 -0400

    feat: finalized styling for social/share module

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,20 @@
-console.log("hello world");
+const socials = document.querySelector("#socials");
+const shareBtn = document.querySelector("#share-btn");
+
+shareBtn.addEventListener("click", function () {
+  if (socials.classList.contains("socials--is-toggled-on")) {
+    socials.classList.remove("socials--is-toggled-on");
+    socials.removeAttribute("role");
+  } else {
+    socials.classList.add("socials--is-toggled-on");
+    socials.setAttribute("role", "alert");
+  }
+
+  if (shareBtn.classList.contains("card__btn--is-toggled-on")) {
+    shareBtn.classList.remove("card__btn--is-toggled-on");
+    shareBtn.setAttribute("aria-pressed", "false");
+  } else {
+    shareBtn.classList.add("card__btn--is-toggled-on");
+    shareBtn.setAttribute("aria-pressed", "true");
+  }
+})
